8304dced79f89ea76789c3fecf0f7ef189e06575
Dzmitry Douhushau <dzmitry.douhushau@softnetix.io>
2024-09-29 18:34:19 +0300

    a

--- linux/nixos/nixos-install/disko.sh
+++ linux/nixos/nixos-install/disko.sh
@@ -2,6 +2,13 @@
 
 (( EUID )) && { echo "NO root"; exit 1; }
 
+if [ -z "$1" ]; then
+  read -sp "Enter root password: " root_password
+  echo
+else
+  root_password="$1"
+fi
+
 tmp_folder="/tmp/nixos"
 disko_link="https://github.com/nix-community/disko.git"
 disko_nix_link="https://raw.githubusercontent.com/ddovguchev/configs/master/linux/nixos/nixos-install/disko.nix"
@@ -11,9 +18,13 @@ mkdir -p "$tmp_folder"
 cd "$tmp_folder" || { echo "Error with $tmp_folder"; exit 1; }
 
 git clone "$disko_link" . || { echo "Error clone REPO disko_link"; exit 1; }
+
 curl -sSL "$disko_nix_link" -o disko.nix || { echo "Error load disko.nix"; exit 1; }
 
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix
 
 nixos-generate-config --root /mnt
+
+echo "root:$root_password" | chpasswd --root /mnt
+
 nixos-install
