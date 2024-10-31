e53cf7c29eb621800befe859998739b21305bd2b
Dzmitry Douhushau <dzmitry.douhushau@softnetix.io>
2024-11-01 02:24:07 +0300

    r

--- linux/nixos/flake/modules/bundel.nix
+++ linux/nixos/flake/modules/bundel.nix
@@ -5,5 +5,6 @@
     ./packages.nix
     ./network-rules.nix
     ./virtmanager.nix
+    ./hyprland.nix
   ];
 }
--- linux/nixos/flake/modules/hyprland.nix
+++ linux/nixos/flake/modules/hyprland.nix
@@ -6,6 +6,10 @@
   # Включаем Hyprland как оконный менеджер
   services.xserver.windowManager.hyprland.enable = true;
 
+  # Устанавливаем Hyprland в качестве менеджера сеансов по умолчанию
+  services.xserver.displayManager.defaultSession = "Hyprland";
+
+
   systemd.user.services.swww-daemon = {
     description = "swww daemon";
     wantedBy = [ "default.target" ];
@@ -14,11 +18,11 @@
     };
   };
 
-  systemd.user.services.hyperland-daemon = {
-    description = "Hyperland daemon";
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.swww}/bin/Hyperland";
-    };
-  };
+#  systemd.user.services.hyperland-daemon = {
+#    description = "Hyperland daemon";
+#    wantedBy = [ "default.target" ];
+#    serviceConfig = {
+#      ExecStart = "${pkgs.swww}/bin/Hyperland";
+#    };
+#  };
 }
