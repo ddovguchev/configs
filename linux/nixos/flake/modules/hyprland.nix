f2b3c85c363e9c544a40087b0d3dc340d1c48bfe
Dzmitry Douhushau <dzmitry.douhushau@softnetix.io>
2024-11-01 02:42:59 +0300

    r

--- linux/nixos/flake/modules/hyprland.nix
+++ linux/nixos/flake/modules/hyprland.nix
@@ -8,17 +8,17 @@
   programs.hyprland.enable = true;
 
   # Устанавливаем Hyprland в качестве менеджера сеансов по умолчанию
-  services.xserver.displayManager.defaultSession = "hyprland";
+#  services.xserver.displayManager.defaultSession = "hyprland";
 
 
-#  systemd.services.swww-daemon = {
-#    description = "swww daemon";
-#    wantedBy = [ "multi-user.target" ];
-#    after = [ "network.target" ];
-#
-#    serviceConfig = {
-#      ExecStart = "${pkgs.swww}/bin/swww daemon";
-#      Restart = "always";
-#    };
-#  };
+  systemd.services.swww-daemon = {
+    description = "swww daemon";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.swww}/bin/swww daemon";
+      Restart = "always";
+    };
+  };
 }
