622fb69857a8d8cbf2d92a84ad9a9463c1619ecd
Dzmitry Douhushau <dzmitry.douhushau@softnetix.io>
2024-10-31 23:52:22 +0300

    r

--- linux/nixos/flake/home-manager/bin/bundle.nix
+++ linux/nixos/flake/home-manager/bin/bundle.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: let
   change_wallpaper = import ./change_wallpaper.nix pkgs;
-  startup = import ./startup.nix pkgs;
-  select_wallpaper = import ./select_wallpaper.nix pkgs;
+#  startup = import ./startup.nix pkgs;
+#  select_wallpaper = import ./select_wallpaper.nix pkgs;
 in [
-  nvidia_offload
   change_wallpaper
-  startup
-  select_wallpaper
+#  startup
+#  select_wallpaper
 ]
