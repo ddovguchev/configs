3539e340d1b36829172b52bca7d1830e9f9ee61b
Dzmitry Douhushau <dzmitry.douhushau@softnetix.io>
2024-11-02 13:48:40 +0300

    r

--- linux/nixos/flake/modules/hyprland.nix
+++ linux/nixos/flake/modules/hyprland.nix
@@ -6,6 +6,7 @@
   services.xserver.enable = false;
 
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.displayManager.defaultSession = "Hyprland";
 
 }
