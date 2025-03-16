b995a17079c3986fb5462e517223e1c4a83a1ec3
dmitriy <ddovguchev@gmail.com>
2025-03-17 01:37:07 +0300

    add darwin

--- darwin/darwin-configuration.nix
+++ darwin/darwin-configuration.nix
@@ -3,8 +3,25 @@
   nixpkgs.hostPlatform = "aarch64-darwin";
 
   environment.systemPackages = with pkgs; [
-    yabai git neovim zsh ripgrep fd bat fzf htop
+    yabai git neovim zsh ripgrep fd bat fzf htop wireguard-tools wireguard-go
+    coreutils air act awscli bun ffmpeg jujutsu gh gnupg go iperf lua-language-server
+    mkalias nil opentofu pass postgresql_16 rclone rustup sqlc stylua stripe-cli
+    tailwindcss tailwindcss-language-server qmk templ tmux zoxide kubectl kubernetes-helm
+    terraform ranger pyenv packer spicetify-cli eza colima k9s tree hcloud talosctl
+    kustomize curl unzip zip wget
   ];
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    brews = [ "mas" "nvm" ];
+    casks = [
+      "hammerspoon" "notion" "iina" "arc" "chatgpt" "intellij-idea" "telegram"
+      "discord" "firefox" "visual-studio-code" "spotify" "gns3" "virtualbox"
+      "altserver" "sony-ps-remote-play"
+    ];
+  };
 
   users.users.dmitriy = {
     name = "dmitriy";
