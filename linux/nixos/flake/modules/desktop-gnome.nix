233525781784370d59d94f99dcdff74dcaf9ad39
dovguchev dmitriy <dzmitry.douhushau@softnetix.io>
2025-04-27 14:45:27 +0300

    add darwin

--- linux/nixos/flake/modules/desktop-gnome.nix
+++ linux/nixos/flake/modules/desktop-gnome.nix
@@ -4,7 +4,7 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
-  systemd.defaultTarget = "graphical.target";
+  systemd.defaultTarget = "graphical";
 
   # Переменные окружения
   environment.variables = {
