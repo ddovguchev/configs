bca21cfde090b6a5e5a649e413a3477ea0af1191
dovguchev dmitriy <dzmitry.douhushau@softnetix.io>
2025-04-27 15:08:15 +0300

    add darwin

--- linux/nixos/flake/modules/desktop-gnome.nix
+++ linux/nixos/flake/modules/desktop-gnome.nix
@@ -6,6 +6,9 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.dbus.enable = true;
 
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Переменные окружения
   environment.variables = {
     EDITOR = "nvim";
