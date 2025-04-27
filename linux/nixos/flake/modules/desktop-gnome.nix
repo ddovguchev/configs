4efae0793a30ad3d16ec75d57f923ae42e7c898d
dovguchev dmitriy <dzmitry.douhushau@softnetix.io>
2025-04-27 15:01:11 +0300

    add darwin

--- linux/nixos/flake/modules/desktop-gnome.nix
+++ linux/nixos/flake/modules/desktop-gnome.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   # Ванильный GNOME
   services.xserver.enable = true;
+  services.xserver.displayManager.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
