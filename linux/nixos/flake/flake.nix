3b67ab72ab93a4ae67923c6665c0007a795ffa94
Dmitriy Dovguchev <dzmitry.douhushau@softnetix.io>
2025-05-11 23:09:23 +0300

    add darwin

--- linux/nixos/flake/flake.nix
+++ linux/nixos/flake/flake.nix
@@ -7,7 +7,7 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
