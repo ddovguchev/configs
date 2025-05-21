7972e279b47044ebbda24546114ce86f3a665b9f
Dmitriy Dovguchev <dzmitry.douhushau@softnetix.io>
2025-05-21 23:00:18 +0300

    add darwin

--- linux/nixos/flake/flake.nix
+++ linux/nixos/flake/flake.nix
@@ -40,6 +40,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.users.hika = import ./home-manager/home.nix;
         }
       ];
